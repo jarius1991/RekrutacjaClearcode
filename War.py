9e3cdd9760c5bf6eca780b7d98814a7b80bc8b2f
jarius1991 <jarius@interia.pl>
2017-02-14 21:12:17 +0100

    Add files via upload

--- War.py
+++ War.py
@@ -1,4 +1,3 @@
-
 def battle(soldiers):
     """
     This function count which fraction win.
@@ -6,9 +5,9 @@ def battle(soldiers):
     :return: String with information who win.
     """
     odds_list, evens_list = assign_squads(soldiers)
-    odds_power=odds(odds_list)
-    evens_power=evens(evens_list)
-    result = choose_winner(odds_power,evens_power)
+    odds_power = odds(odds_list)
+    evens_power = evens(evens_list)
+    result = choose_winner(odds_power, evens_power)
     return result
 
 
@@ -18,20 +17,19 @@ def assign_squads(soldiers):
     :param soldiers: Table of numbers which represents soldiers from Odds and Evens fraction.
     :return: Two tables with Odds soldiers and Evens soldiers.
     """
-    odds_list=[]
-    evens_list=[]
+    odds_list = []
+    evens_list = []
     for squad in soldiers:
-        if squad==0:
+        if squad == 0:
             pass
-        elif squad%2==0:
+        elif squad % 2 == 0:
             evens_list.append(squad)
         else:
             odds_list.append(squad)
     return odds_list, evens_list
 
 
-
-def choose_winner(odd_num,even_num):
+def choose_winner(odd_num, even_num):
     """
     This function compare which fraction have more power.
     :param odd_num: Power of Odds fraction.
@@ -39,12 +37,12 @@ def choose_winner(odd_num,even_num):
     :return: String with result of battle.
     """
 
-    if odd_num>even_num:
-        result="odds win"
-    elif odd_num<even_num:
-        result="evens win"
+    if odd_num > even_num:
+        result = "odds win"
+    elif odd_num < even_num:
+        result = "evens win"
     else:
-        result="tie"
+        result = "tie"
 
     return result
 
@@ -55,7 +53,7 @@ def odds(soldiers):
     :param soldiers: Table of numbers symbolizing Odds soldiers.
     :return: Power of Odds fraction.
     """
-    return count_soldiers_power(soldiers,1)
+    return count_soldiers_power(soldiers, 1)
 
 
 def evens(soldiers):
@@ -64,36 +62,36 @@ def evens(soldiers):
     :param soldiers: Table of numbers symbolizing Evens soldiers.
     :return: Power of Evens fraction
     """
-    return count_soldiers_power(soldiers,0)
+    return count_soldiers_power(soldiers, 0)
 
 
-def count_soldiers_power(soldiers,type):
+def count_soldiers_power(soldiers, war_number):
     """
     Count how many soldiers and spies/saboteurs have given squad.
     :param soldiers: Table of numbers symbolizing soldiers.
-    :param type: Number which symbolize fighting symbol.
+    :param war_number: Number which symbolize fighting symbol.
     :return: Power of soldiers squads.
     """
-    power=0
+    power = 0
     for soldier in soldiers:
-        power += count_power(soldier,type)
+        power += count_power(soldier, war_number)
     return power
 
 
-def count_power(soldier,type):
+def count_power(soldier, war_number):
     """
     Count power of given soldier.
     :param soldier: Number which symbolise soldier in squad.
-    :param type: Number which symbolize fighting symbol.
+    :param war_number: Number which symbolize fighting symbol.
     :return:  Power of given squad.
     """
-    if soldier<0:
-       return - bin(soldier)[3:].count(str(type)) # Solve saboteur and spies problem
+    if soldier < 0:
+        return - bin(soldier)[3:].count(str(war_number))  # Solve saboteur and spies problem
     else:
-       return bin(soldier)[2:].count(str(type))
+        return bin(soldier)[2:].count(str(war_number))
 
 
-if __name__=="__main__":
-    print(battle([21,-3,20]))
-    print(battle([7,-3,-14,6]))
-    print(battle([23,-3,32,-24]))
+if __name__ == "__main__":
+    print(battle([21, -3, 20]))
+    print(battle([7, -3, -14, 6]))
+    print(battle([23, -3, 32, -24]))
